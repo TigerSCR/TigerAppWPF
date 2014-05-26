c968478bb9088b504540efc27b5f720a46f770f6
RomainDAV <romain.daversa@gmail.com>
2014-05-26 15:39:42 +0200

    Finalisé

--- TigerAppWPF/ModuleConcentration.cs
+++ TigerAppWPF/ModuleConcentration.cs
@@ -27,16 +27,16 @@ namespace TigerAppWPF
             result = 0;
             total=source.Count;
         }
-        /*
+        
         protected override void calculate(List<Title> source)
         {
             //répartition par parent
             foreach (Title t in source)
             {
-                if (listByCorporate.Keys.Contains(t.Parent))
-                    listByCorporate[t.Parent].Add(t);
+                if (listByCorporate.Keys.Contains(t.GetID_Corp))
+                    listByCorporate[t.GetID_Corp].Add(t);
                 else
-                    listByCorporate.Add(t.Parent, t);
+                    listByCorporate.Add(t.GetID_Corp, new List<Title>() {t});
             }
             //supression des titres uniques
             foreach(var v in listByCorporate)
@@ -56,7 +56,7 @@ namespace TigerAppWPF
                 foreach(Title t in v.Value)
                 {
                     somme+=t.Total;
-                    rating+=t.Rate*t.Total;
+                    rating+=t.GetRating*t.Total;
                 }
                 rating/=somme;
                 double xsi =  Math.Max(0, (somme / total) - ct[(int)rating]);
@@ -66,10 +66,12 @@ namespace TigerAppWPF
 
             //calcul final
             foreach (var v in totalByCorporate)
-            { 
-                result+=
+            {
+                result += v.Value.Item4*v.Value.Item4;//somme les conci au carré
             }
-        }*/
+            // prends la racine
+            result = Math.Sqrt(result);
+        }
 
         public double Result
         { get { return this.result; } }
